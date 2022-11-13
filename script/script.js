82cc7bf0b365f078557bcaffce5f4d9965a47635
Banky <kingmorty@protonmail.com>
2022-11-13 16:01:57 +0100

    Change event listener logic for all buttons

--- script/script.js
+++ script/script.js
@@ -36,12 +36,24 @@ function operate (operator, num1, num2) {
 }
 
 function appendDisplay (text) {
-  displyArea.textContent += text;
+  displayValue = displyArea.textContent += text;
 }
 
 const displyArea = document.querySelector("#display textarea");
-const numberButtons = document.querySelectorAll("#num-buttons button");
+const allButtons = document.querySelectorAll(".container button")
+// const operators = document.querySelectorAll("#operators button");
+// const numberButtons = document.querySelectorAll("#num-buttons button");
 
-for (let btn of numberButtons) {
+let displayValue;
+
+for (let btn of allButtons) {
   btn.addEventListener("click", () => appendDisplay(btn.textContent));
 }
+
+// for (let btn of numberButtons) {
+//   btn.addEventListener("click", () => appendDisplay(btn.textContent));
+// }
+
+// for (let btn of operators) {
+//   btn.addEventListener("click", () => appendDisplay(btn.textContent));
+// }
